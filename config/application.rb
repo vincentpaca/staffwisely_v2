7a7d4c64a1bc01cac1988363e989622621077f90
Vincent Paca <vincent.paca@gmail.com>
2013-01-29 00:04:50 +0800

    Updated config environments

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module StaffwiselyV2
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
+
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
