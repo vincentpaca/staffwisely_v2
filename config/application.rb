e5e1fce92e6e373e19e6bc7bd7ad262b05dd9a2b
Vincent Paca <vincent.paca@gmail.com>
2013-01-30 23:41:54 +0800

    Use different layout for devise

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,15 @@ module StaffwiselyV2
     config.assets.initialize_on_precompile = false
 
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Devise layout
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
   end
 end
