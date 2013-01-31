12981f53c995960c3af9adb5401e7f8e2a4bdd75
Vincent Paca <vincent.paca@gmail.com>
2013-01-31 23:55:39 +0800

    Made draft accessible

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :contact_details, :description, :requirements, :title, :working_arrangement, :experience, :user
+  attr_accessible :contact_details, :description, :requirements, :title, :working_arrangement, :experience, :user, :draft
 
   belongs_to :user
 end
