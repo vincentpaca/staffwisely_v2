a1977139110b7a3d27994048e77a80eaaa4e7c2c
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 07:13:41 +0800

    Added show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,13 @@ class JobsController < DashboardController
   def index
     redirect_to "/dashboard"
   end
+
+  def show
+    @job = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
