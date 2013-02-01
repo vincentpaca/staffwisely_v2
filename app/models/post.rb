21934f407524119531d58caa369778ef6c6460d8
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 19:01:17 +0800

    Added relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :contact_details, :description, :requirements, :title, :working_arrangement, :experience, :user, :draft
 
   belongs_to :user
+  has_many :proposals
 
   def active
     Post.where(:draft => false)
