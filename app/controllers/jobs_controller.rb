0a9f2810e35bdbd86f54f8c76e281bb8f5c6393e
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 19:35:34 +0800

    Added create action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -20,4 +20,19 @@ class JobsController < DashboardController
       format.js
     end
   end
+
+  def create
+    @job = Post.new(params[:post])
+    @job.user = current_user
+
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to "/dashboard", :notice => t("jobs.new.success") }
+        format.js { render :json => @job, :status => :created }
+      else
+        format.html { render :action => "new", :notice => t("jobs.new.success") }
+        format.js { render :json => @job.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
