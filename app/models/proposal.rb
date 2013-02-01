cb243a6a9b7a99ddca247164d0f95f386d9982b5
Vincent Paca <vincent.paca@gmail.com>
2013-02-02 00:38:59 +0800

    Added proposal form

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,5 +1,5 @@
 class Proposal < ActiveRecord::Base
-  attr_accessible :currency, :details, :hourly_rate, :monthly_rate, :post_id, :user_id
+  attr_accessible :currency, :details, :hourly_rate, :monthly_rate, :post_id, :user_id, :monthly_currency, :hourly_currency
 
   belongs_to :user
   belongs_to :post
