1afae8c4b6b56e337782109fd9307fa99f4fedd3
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 21:43:25 +0800

    Added company model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201133225) do
+ActiveRecord::Schema.define(:version => 20130201134243) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
