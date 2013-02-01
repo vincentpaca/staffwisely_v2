2fda47133455fe78e48421d15536210088f835d7
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 18:47:39 +0800

    Added new proposal page

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,10 @@
-class ProposalsController < ApplicationController
+class ProposalsController < DashboardController
+  def new
+    @proposal = Proposal.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
