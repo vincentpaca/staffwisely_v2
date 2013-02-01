98e2f0ad00d86536991f6afecce4de5b6ebc0f78
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 21:34:06 +0800

    Added default value for draft in posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201105817) do
+ActiveRecord::Schema.define(:version => 20130201133225) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.text     "requirements"
     t.text     "contact_details"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.integer  "working_arrangement"
     t.integer  "experience"
-    t.boolean  "draft"
+    t.boolean  "draft",               :default => false
     t.integer  "user_id"
   end
 
