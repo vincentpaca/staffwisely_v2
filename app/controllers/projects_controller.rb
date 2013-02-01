f75d488ee526e9b6eaf1053b39a8a6559e3e10dc
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 21:41:53 +0800

    Added single project view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,19 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Post.active
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def show
+    @project = Post.includes(:user).find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
