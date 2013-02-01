05aa3a36a064443bd40b36ad387d8ae847df4996
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 11:53:55 +0800

    Added static classes

---
+++ config/initializers/ambry.rb
@@ -0,0 +1,7 @@
+WorkingArrangement.create :id => 1, :name => "On-site"
+WorkingArrangement.create :id => 2, :name => "Remote with Occassional Meetup"
+WorkingArrangement.create :id => 3, :name => "Full Remote"
+
+Experience.create :id => 1, :name => "Junior"
+Experience.create :id => 2, :name => "Middle"
+Experience.create :id => 3, :name => "Senior"
