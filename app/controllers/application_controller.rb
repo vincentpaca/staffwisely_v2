23bafda214afda1ae9e13821d843fb7e3e1fd0cb
Vincent Paca <vincent.paca@gmail.com>
2013-02-01 18:44:11 +0800

    Added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def work_arrangements
+    WorkingArrangement.all
+  end
+  helper_method :work_arrangements
+
+  def experiences
+    Experience.all
+  end
+  helper_method :experiences
 end
