347c2f7ceef6a05fc6befdab6b8fb0bde367479f
Vincent Paca <vincent.paca@gmail.com>
2013-02-02 10:01:01 +0800

    Added submission of proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,10 +1,17 @@
 class ProposalsController < DashboardController
-  def new
-   # @proposal = Proposal.new
+  def create
+    @proposal = Proposal.new(params[:proposal])
+    @proposal.user = current_user
+    @proposal.company = current_user.company
 
     respond_to do |format|
-      format.html
-      format.js
+      if @proposal.save
+        format.html { redirect_to projects_path, :notice => t("proposals.new.success") }
+        format.js { render :json => @proposal, :status => :created }
+      else
+        format.html { redirect_to project_path(@proposal.post), :alert => t("proposals.new.fail") }
+        format.js { render :json => @proposal.errors, :status => :unprocessable_entity }
+      end
     end
   end
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,7 +1,9 @@
 class Proposal < ActiveRecord::Base
-  attr_accessible :currency, :details, :hourly_rate, :monthly_rate, :monthly_currency, :hourly_currency, :user, :post, :company
+  attr_accessible :currency, :details, :hourly_rate, :monthly_rate, :monthly_currency, :hourly_currency, :user, :post, :company, :post_id
 
   belongs_to :user
   belongs_to :company
   belongs_to :post
+
+  validates_presence_of :user, :company, :post_id, :details, :monthly_rate, :hourly_rate
 end
