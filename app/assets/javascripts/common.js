d8c8e51ea6dedbaf662e65ea054f21581f761d9f
Harvey Enrile <harvey.enrile@gmail.com>
2013-02-01 19:54:30 -0500

    updated propose to job page

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$(".item-round").hover(function(){
 		$(this).find('.tooltip').fadeToggle('fast');
 	});
+	
+	$("#proposeBtn").click(function(e){
+		e.preventDefault();
+		$(".submit-proposal").slideDown('fast');
+		$('html, body').animate({ scrollTop: $('.submit-proposal').offset().top }, 'fast');
+	});
 });
