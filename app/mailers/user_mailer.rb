5f9f85c324eb17b431d2d1e3498f44e7bd9a2a54
Vincent Paca <vincent.paca@gmail.com>
2013-02-04 16:21:40 +0800

    Updated mailer senders

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
-  default from: "hello@staffwisely.com"
+  default from: "\"Harry Arbasa\" <harry@staffwisely.com>"
 
   def welcome(email)
-    mail(:to => email, :subject => "w00t! Welcome to Staffwisely")
+    mail(:to => email, :subject => "Good Call! Welcome to Staffwisely!")
   end
 end
